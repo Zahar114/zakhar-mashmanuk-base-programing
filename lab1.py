10df6c5bb0a4854fa25b9ea6e8961e28eef04af3
Zahar114 <162366582+Zahar114@users.noreply.github.com>
2025-09-04 00:07:27 +0300

    lab1

---
+++ lab1.py
@@ -0,0 +1,17 @@
+age = 16
+height = 1.70
+name = "Zahar"
+is_student = True
+grades = [90, 85, 100]
+coordinates = (50, 30)
+person = {"name": "Zahar", "age": 16}
+unique_numbers = {1, 2, 3}
+
+print("age",type(age),age)
+print("height,", type(height), height)
+print("name,", type(name), name)
+print("is_student,", type(is_student), is_student)
+print("grades,", type(grades), grades)
+print("coordinates,", type(coordinates), coordinates)
+print("person,", type(person), person)
+print("unique_numbers,", type(unique_numbers), unique_numbers)
